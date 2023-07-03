75edd278ef4ef4655817507fc930e736f1b83ebf
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-03 19:39:13 +0100

    pages created

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,8 +1,35 @@
+import "../App.css";
 import React from 'react'
-
+import logo from "../moralis-logo.svg"
+import Eth from "../eth.svg"
+import { Link } from "react-router-dom"
 function Header() {
+  function connect() {
+
+  }
   return (
-    <div>Header</div>
+    <div>
+      <div className='leftH'>
+        <img src={logo} alt='logo' className='logo' />
+        <Link to="/" className='link'>
+          <div className='headerItem'>Swap</div>
+        </Link>
+        <Link to="tokens" className='link'>
+          <dic className="headerItem">Tokens</dic>
+        </Link>
+
+      </div>
+
+      <div className='rightH'>
+        <div className='headerItem'>
+          <img className='eth' src={Eth} />
+          Ethereum
+        </div>
+        <button className='connectButton' onClick={connect}>
+          connect
+        </button>
+      </div>
+    </div>
   )
 }
 
--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Input, Popover, Radio, Modal, meassage } from "antd"
+import { ArrowDownOutlined, DownOutlined, SettingOutlined, } from "@ant-design/icons";
 
 function Swap() {
+  const [slippage, setSlippage] = useState(0.5);
+  const [tokenOneAmount,setTokenOneAmount] = useState("");
+  const [tokenTwoAmount, setTokenTwoAmount] = useState("");
+
+  function changeAmonut(e){
+    setTokenOneAmount(e.target.value)
+  }
+
+  function handleSlippage(e){
+    setSlippage(e.target.value)
+  }
+  const settings = (
+    <>
+    <div>
+      <Radio.Group value={slippage} onChange={handleSlippage}>
+        <Radio.Button value={0.5}>0.5</Radio.Button>
+        <Radio.Button value={2.0}>2.0</Radio.Button>
+        <Radio.Button value={3.5}>3.5</Radio.Button>
+        <Radio.Button value={5}>5.0</Radio.Button>
+      </Radio.Group>
+    </div>
+    </>
+  );
   return (
-    <div>Swap</div>
+    <div className='tradeBox'>
+      <div className='tradeBoxHeader'>
+        <h4>Swap</h4>
+        <Popover 
+        title="Settings"
+        trigger="click"
+        placement='bottomRight'
+        content={settings}
+        >
+        <SettingOutlined className='cog' />
+        </Popover>
+      </div>
+      <div className='inputs'>
+        <Input placeholder='0' value={tokenOneAmount} onChange={changeAmonut} />
+        <Input placeholder='0' value={tokenTwoAmount} disabled= {true} />
+        <div className='asssetOne'></div>
+        <div className='asssetTwo'></div>
+      </div>
+    </div>
   )
 }
 
