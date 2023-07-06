6a76da50817ce29fa3f4767cfb55795acfdaaf4f
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-06 14:40:50 +0100

    set up wagmi

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -3,34 +3,59 @@ import React from 'react'
 import logo from "../moralis-logo.svg"
 import Eth from "../eth.svg"
 import { Link } from "react-router-dom"
-function Header() {
-  function connect() {
 
-  }
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+
+
+
+
+function Header() {
+
+  const { address, isConnected } = useAccount()
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  })
+  const { disconnect } = useDisconnect()
+
+  // function connect() {
+
+  // }
+
+  
+  // return (
+  //   <div>
+  //     <div className='leftH'>
+  //       <img src={logo} alt='logo' className='logo' />
+  //       <Link to="/" className='link'>
+  //         <div className='headerItem'>Swap</div>
+  //       </Link>
+  //       <Link to="tokens" className='link'>
+  //         <dic className="headerItem">Tokens</dic>
+  //       </Link>
+
+  //     </div>
+
+  //     <div className='rightH'>
+  //       <div className='headerItem'>
+  //         <img className='eth' src={Eth} />
+  //         Ethereum
+  //       </div>
+  //       <button className='connectButton' onClick={connect}>
+  //         connect
+  //       </button>
+  //     </div>
+  //   </div>
+  // )
+  if (isConnected)
   return (
     <div>
-      <div className='leftH'>
-        <img src={logo} alt='logo' className='logo' />
-        <Link to="/" className='link'>
-          <div className='headerItem'>Swap</div>
-        </Link>
-        <Link to="tokens" className='link'>
-          <dic className="headerItem">Tokens</dic>
-        </Link>
-
-      </div>
-
-      <div className='rightH'>
-        <div className='headerItem'>
-          <img className='eth' src={Eth} />
-          Ethereum
-        </div>
-        <button className='connectButton' onClick={connect}>
-          connect
-        </button>
-      </div>
+      Connected to {address}
+      <button onClick={() => disconnect()}>Disconnect</button>
     </div>
   )
+return <button onClick={() => connect()}>Connect Wallet</button>
+
 }
 
 export default Header
--- dex/src/index.js
+++ dex/src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { WagmiConfig, createClient } from 'wagmi'
+import { getDefaultProvider } from 'ethers'
+
+const client = createClient({
+  autoConnect: true,
+  provider: getDefaultProvider(),
+})
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <WagmiConfig client={client}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </WagmiConfig>
   </React.StrictMode>
 );
